01dec3f8160272ba97431f8d5062f765023a778e
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-02 11:41:15 -0300

    Correção de português :P

--- questao10.py
+++ questao10.py
@@ -4,7 +4,7 @@ valor	+	(valor	x	(taxa	:	100)	x	tempo	em	dias)
 '''
 
 valor = float(input("Qual o valor original?: "))
-taxa = float(input("Qual é a taxa?: "))
-tempo = int(input("Qual o tempo em dias?: "))
+taxa = float(input("Qual é a taxa de juro?: "))
+tempo = int(input("Qual o tempo, em dias, de atraso?: "))
 
 print(f'\nValor da prestação em atraso: R${valor + (valor * (taxa / 100) * tempo)}')
