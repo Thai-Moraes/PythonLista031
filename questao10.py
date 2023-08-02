644b02848476e88482a484ea6dd5fe7b4a36b604
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-02 11:33:26 -0300

    Mudança de 'float' para 'inteiro'

--- questao10.py
+++ questao10.py
@@ -5,6 +5,6 @@ valor	+	(valor	x	(taxa	:	100)	x	tempo	em	dias)
 
 valor = float(input("Qual o valor original?: "))
 taxa = float(input("Qual é a taxa?: "))
-tempo = float(input("Qual o tempo em dias?: "))
+tempo = int(input("Qual o tempo em dias?: "))
 
 print(f'\nValor da prestação em atraso: R${valor + (valor * (taxa / 100) * tempo)}')
